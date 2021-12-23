1dc085f2db87b1d2fad4d22eaa4a60229653f30c
Daniel Lauzon <daniel.lauzon@gmail.com>
2021-12-23 15:50:03 -0500

    Fix: linting

--- src-js/index.js
+++ src-js/index.js
@@ -39,49 +39,48 @@ async function main () {
   const uploads = await storage.list()
 
   // Below is the code to delete previous uploads (keeping N most recent)
-  const numberToKeep = 3;
-  await deletePreviousUploads(uploads,feedFilename,numberToKeep);
-
+  const numberToKeep = 3
+  await deletePreviousUploads(uploads, feedFilename, numberToKeep)
 }
 
 main()
 
 // upload item comparator ( for sorting upload items by descending created timestamp)
-function createdDescendingStampComparator(a, b) {
+function createdDescendingStampComparator (a, b) {
   // The typical sign of return is reversed because the comparator is "Descending"
-  if(a?.created < b?.created) {
-    return 1;
+  if (a?.created < b?.created) {
+    return 1
   }
-  if(a?.created > b?.created) {
-    return -1;
+  if (a?.created > b?.created) {
+    return -1
   }
   // a must be equal to b
-  return 0;
+  return 0
 }
 
 // uploads is an asyncIterable of upload items
-async function deletePreviousUploads(uploads,prefixUploadName,numberToKeep) {
-  console.log(`- Delete previous uploads matching ${prefixUploadName}*, keeping ${numberToKeep} entries`);
+async function deletePreviousUploads (uploads, prefixUploadName, numberToKeep) {
+  console.log(`- Delete previous uploads matching ${prefixUploadName}*, keeping ${numberToKeep} entries`)
 
   // filter for files uploaded by this script (prefix of feedFilename)
-  const matchingUploads = [];
-  for await(const upload of uploads) {
-    if(upload?.name.startsWith(prefixUploadName)) {
-      matchingUploads.push(upload);
+  const matchingUploads = []
+  for await (const upload of uploads) {
+    if (upload?.name.startsWith(prefixUploadName)) {
+      matchingUploads.push(upload)
     } else {
-      console.log(`Excluding ${upload.name} CID: ${upload.cid}`);
+      console.log(`Excluding ${upload.name} CID: ${upload.cid}`)
     }
   }
-  console.log(`- Found ${matchingUploads.length} matches`);
+  console.log(`- Found ${matchingUploads.length} matches`)
 
   // sort matchingUploads in place, but give it a new symbol for clarity
-  const sortedDescendingMatchingUploads = matchingUploads.sort(createdDescendingStampComparator);
+  const sortedDescendingMatchingUploads = matchingUploads.sort(createdDescendingStampComparator)
 
   // keep first 'keepers' entries (delete all others)
-  const toDelete = sortedDescendingMatchingUploads.slice(numberToKeep);
-  console.log(`Keeping ${numberToKeep} entries, about to delete ${toDelete.length} entries`);
-  for(const upload of toDelete) {
-    console.log(`Would delete ${upload.name} CID: ${upload.cid}`);
+  const toDelete = sortedDescendingMatchingUploads.slice(numberToKeep)
+  console.log(`Keeping ${numberToKeep} entries, about to delete ${toDelete.length} entries`)
+  for (const upload of toDelete) {
+    console.log(`Would delete ${upload.name} CID: ${upload.cid}`)
     // .delete not implemented yet
     // const deletedCID = await storage.delete(upload.cid)
     // console.log(`Deleted CID: ${deletedCID}`)
