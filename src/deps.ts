da73ca81049d4828b0952f78e79f843dd810283d
Daniel Lauzon <daniel.lauzon@gmail.com>
2023-04-24 15:56:52 -0400

    Disable pinning imports in deps.tx

--- src/deps.ts
+++ src/deps.ts
@@ -4,8 +4,8 @@ export { ensureDir } from "https://deno.land/std@0.182.0/fs/mod.ts";
 export { parseFeed } from "https://deno.land/x/rss@0.5.8/mod.ts";
 
 // for pin
-export {
-  // getFilesFromPath, BROKEN!!!!!!!!!!!!!
-  File,
-  Web3Storage,
-} from "https://esm.sh/web3.storage@4.5.4/";
+// export {
+//   // getFilesFromPath, BROKEN!!!!!!!!!!!!!
+//   File,
+//   Web3Storage,
+// } from "https://esm.sh/web3.storage@4.5.4/";
