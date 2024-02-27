ca5d6d8f0338092d1f7c040acf6c4f882045bfa4
Daniel Lauzon <daniel.lauzon@gmail.com>
2024-02-27 01:01:44 -0500

    Update dependencies in pin and scrape apps

--- apps/scrape/src/deps.ts
+++ apps/scrape/src/deps.ts
@@ -1,4 +1,4 @@
 // for scrape
-export { join } from "https://deno.land/std@0.184.0/path/mod.ts";
-export { ensureDir } from "https://deno.land/std@0.184.0/fs/mod.ts";
-export { parseFeed } from "https://deno.land/x/rss@0.5.8/mod.ts";
+export { join } from "https://deno.land/std@0.217.0/path/mod.ts";
+export { ensureDir } from "https://deno.land/std@0.217.0/fs/mod.ts";
+export { parseFeed } from "https://deno.land/x/rss@1.0.0/mod.ts";
