d9fae263d1ade4fa9ab905287f1db2689217c2bb
Daniel Lauzon <daniel.lauzon@gmail.com>
2024-02-27 00:23:37 -0500

    typo in script

--- apps/pin-sh/uploadToW3Storage.sh
+++ apps/pin-sh/uploadToW3Storage.sh
@@ -65,8 +65,9 @@ commands_exist npm node jq w3
 env_vars_exist W3_PRINCIPAL W3_PROOF
 
 # add our proof
-export W3_PROOF=$W3_PROOF
 print_header "Adding proof to gain access to w3 space (books)"
+echo "  principal: |$W3_PRINCIPAL|"
+echo "  proof: |$W3_PROOF|"
 w3 space add $W3_PROOF
 
 print_header "Showing added proof and capabilities"
